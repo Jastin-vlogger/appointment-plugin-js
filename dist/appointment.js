8cfa132d16cb65ad2a0ced0d9446c9fc92d48eba
Jastin-vlogger <justinkj765@gmail.com>
2025-03-17 13:03:38 +0530

    commiting dist files

---
+++ dist/appointment.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.AppointmentPlugin=o():e.AppointmentPlugin=o()}(self,(()=>({})));
